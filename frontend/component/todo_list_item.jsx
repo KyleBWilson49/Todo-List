5ae180d0554e4dad3bf62819b75f00f7191938db
App Academy Student <student@appacademy.io>
2015-12-08 14:27:46 -0800

    finished phase 4

--- frontend/component/todo_list_item.jsx
+++ frontend/component/todo_list_item.jsx
@@ -2,11 +2,17 @@ var React = require('react');
 var TodoStore = require('../stores/todo_store.js');
 
 var TodoListItem = React.createClass({
+  handleDestroy: function() {
+    TodoStore.destroy(this.props.item.id);
+  },
   render: function() {
     return (
       <div>
         <div>
           {this.props.item.title}
+          <button onClick={this.handleDestroy}>
+            Delete
+          </button>
         </div>
         <div>
           {this.props.item.body}
