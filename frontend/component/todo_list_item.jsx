ae9140d645be4d1d58c605a6b4a9791589bc7ad6
App Academy Student <student@appacademy.io>
2015-12-08 14:35:54 -0800

    finished phase 5

--- frontend/component/todo_list_item.jsx
+++ frontend/component/todo_list_item.jsx
@@ -5,7 +5,24 @@ var TodoListItem = React.createClass({
   handleDestroy: function() {
     TodoStore.destroy(this.props.item.id);
   },
+
+  handleDone: function() {
+    TodoStore.toggleDone(this.props.item.id);
+  },
+
   render: function() {
+    var doneButton;
+
+    if (!this.props.item.done) {
+      doneButton = (
+          <button onClick={this.handleDone}>
+            Done
+          </button>
+      );
+    } else {
+      doneButton = "";
+    }
+
     return (
       <div>
         <div>
@@ -16,6 +33,7 @@ var TodoListItem = React.createClass({
         </div>
         <div>
           {this.props.item.body}
+          {doneButton}
         </div>
       </div>
     );
